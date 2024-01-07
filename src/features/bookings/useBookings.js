4dff6c96275f5e00a1378aa3761e447933587878
eniola <eniolafaithful57@gmail.com>
2024-01-07 18:07:36 +0100

    improve the booking fetch function architecture to be more dynamic

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter = !filterValue || filterValue === 'all' ? null : { field: "status", value: filterValue }
+
+  const sortByRaw = searchParams.get("SortBy") || "startDate-asc";
+  const [sortValue, order] = sortByRaw.split("-");
+
+  const sortBy = { field: "SortBy", value: sortValue, order };
+
   const { isLoading, error, data } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings(filter, sortBy),
   });
 
   return { isLoading, error, data };
