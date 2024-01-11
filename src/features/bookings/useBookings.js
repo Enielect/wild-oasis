05d42a2b1ccc68cc390fe1c1917ab86b202e7b75
eniola <eniolafaithful57@gmail.com>
2024-01-12 00:05:11 +0100

    implemented prefetching of data using the prefetQuery from react Query

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import {
+  QueryClientProvider,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { ITEMS_PER_PAGE } from "../../utils/pagination-util";
 
 function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   const pageIndex = searchParams.get("page") || 1;
   const filterValue = searchParams.get("status");
@@ -17,6 +23,7 @@ function useBookings() {
 
   const sortBy = { field: "SortBy", value: sortValue, order };
 
+  // FETCHING QUERIES
   const {
     isLoading,
     error,
@@ -25,6 +32,17 @@ function useBookings() {
     queryKey: ["bookings", filter, sortBy, pageIndex],
     queryFn: () => getBookings(filter, sortBy, pageIndex),
   });
+
+  // PRE-FETCHING QUERIES
+  if (pageIndex < count / ITEMS_PER_PAGE)
+    queryClient.prefetchQuery(["bookings", filter, sortBy, pageIndex + 1], () =>
+      getBookings(filter, sortBy, pageIndex + 1)
+    );
+
+  if (pageIndex > 1)
+    queryClient.prefetchQuery(["bookings", filter, sortBy, pageIndex - 1], () =>
+      getBookings(filter, sortBy, pageIndex - 1)
+    );
 
   return { isLoading, error, bookings, count };
 }
