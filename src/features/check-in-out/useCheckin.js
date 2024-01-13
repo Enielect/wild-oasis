e11a3b1e5c6242b63e6cd90e5ba91efde005eee3
eniola <eniolafaithful57@gmail.com>
2024-01-13 23:21:05 +0100

    implementing checkin confirmation functionality

---
+++ src/features/check-in-out/useCheckin.js
@@ -0,0 +1,27 @@
+import { useMutation } from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+function useCheckin() {
+    const navigate = useNavigate();
+
+  const { mutate: checkIn, isLoading: isChecking } = useMutation({
+    mutationFn: ({ bookingId, breakfast }) => {
+      updateBooking(bookingId, {
+        status: "checked-in",
+        isPaid: true,
+        ...breakfast
+      },
+
+      onSuccess: (data) => {
+        navigate('/');
+        toast.success('successfully checked-in')
+      }
+      );
+    },
+  });
+}
+
+
+export default useCheckin
