f0d7d8867ec1932c0582b6dd4f76064fad4555cd
eniola <eniolafaithful57@gmail.com>
2024-01-13 01:35:46 +0100

    removed the bookingId page from the Bookings nested component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Booking from "./pages/Booking";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,7 +21,6 @@ const queryClient = new QueryClient({
     },
   },
 });
-
 
 function App() {
   return (
@@ -32,7 +32,9 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="account" element={<Account />} />
             <Route path="dashboard" element={<Dashboard />} />
+
             <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="settings" element={<Settings />} />
             <Route path="users" element={<Users />} />
@@ -46,5 +48,6 @@ function App() {
     </QueryClientProvider>
   );
 }
+
 
 export default App;
